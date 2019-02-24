c46c4a6c302e6529184aaa4c8346f0fdc55da1d5
Nicholas Carnival <ncarnival2@yahoo.com>
2019-02-24 01:53:54 -0700

    attempt at outbound calls

--- responseTest.py
+++ responseTest.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -8,6 +9,17 @@ message = "Hello, This is a Test. Electric Boogaloo!"
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     """Respond to incoming phone calls with a 'Hello world' message"""
+    account_sid = ''
+    auth_token = ''
+    client = Client(account_sid, auth_token)
+
+    call - client.calls.create (
+            url = 'twiml.xml',   
+            to='+17205142454',
+            from_='+17207704132'
+                    )
+
+    print(call.sid)
     # Start our TwiML response
     resp = VoiceResponse()
 
@@ -15,6 +27,7 @@ def voice():
     resp.say(message)
 
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
