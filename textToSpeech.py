909c7bbb80b47b3e76660c14eb7b93b132203100
arthurmayer <arthurmayer@mymail.mines.edu>
2019-02-23 23:38:24 -0800

    Play music

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,5 +1,13 @@
+import contextlib #To supress output of pygame import
+with contextlib.redirect_stdout(None): #Supress output of pygame import
+    from pygame import mixer #Playing music
+    import pygame
 from gtts import gTTS
 import os
 tts = gTTS(text='Good morning I am a sad robittt oh whale', lang='en')
-tts.save("good.mp3")
-os.system("mpg123 good.mp3")
+tts.save("good.wav")
+#os.system("mpg123 good.mp3")
+
+mixer.init()
+mixer.music.load(good.wav)
+mixer.music.play()
