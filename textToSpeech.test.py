eed965aca82c2f7caab5b97291fe78efeeb9490b
Nicholas Carnival <ncarnival2@yahoo.com>
2019-02-24 00:56:04 -0700

    textToSPeech.test.py

---
+++ textToSpeech.test.py
@@ -0,0 +1,20 @@
+import os 
+import platform
+import sys
+
+operatingSystem = sys.platform
+print ("Operating System: " + operatingSystem)
+
+words = 'Good morning I am a sad robittt oh whale'
+
+if (operatingSystem = 'linux'):
+    from gtts import gTTS
+    tts = gTTS(text=words, lang='en')
+
+    tts.save("good.wav")
+    os.system("mpg123 good.wav")
+if (operatingSystem = 'win32'):
+    import pyttsx
+    engine = pyttsx.init()
+    engine.say(words)
+    engine.runAndWait()
