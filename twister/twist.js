fefd7528ca8fa6bab1cb8ba4b68acfca280ce591
Nicholas Carnival <ncarnival2@yahoo.com>
2019-02-24 11:01:10 -0700

    finished twsit bot

--- twister/twist.js
+++ twister/twist.js
@@ -1,11 +1,22 @@
-const bodyParser = require('body-parser')
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const app = express();
+
+// Parse POST requests with JSON or URLEncoded
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server listening on port ${process.env.PORT || 3000}`),
+);
+
+app.post('/bot', (req, res) => {
+  const body = req.body;
+  console.log(body);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
