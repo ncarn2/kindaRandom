e03678252f52103b67f18561f45cbb94395fc634
Kai Mizuno <Mizuno.PK@gmail.com>
2019-02-24 01:27:02 -0700

    Initial script for calling a number and hearing a message

---
+++ responseTest.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from twilio.twiml.voice_response import VoiceResponse
+
+app = Flask(__name__)
+
+message = "Hello, This is a Test. Electric Boogaloo!"
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    """Respond to incoming phone calls with a 'Hello world' message"""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # Read a message aloud to the caller
+    resp.say(message)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
