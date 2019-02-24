5fea278baa1bb55b39351c1b909085f628b7c75f
Nicholas Carnival <ncarnival2@yahoo.com>
2019-02-24 00:57:00 -0700

    forogt ==

--- textToSpeech.test.py
+++ textToSpeech.test.py
@@ -7,13 +7,13 @@ print ("Operating System: " + operatingSystem)
 
 words = 'Good morning I am a sad robittt oh whale'
 
-if (operatingSystem = 'linux'):
+if (operatingSystem == 'linux'):
     from gtts import gTTS
     tts = gTTS(text=words, lang='en')
 
     tts.save("good.wav")
     os.system("mpg123 good.wav")
-if (operatingSystem = 'win32'):
+if (operatingSystem == 'win32'):
     import pyttsx
     engine = pyttsx.init()
     engine.say(words)
